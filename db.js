f3805d4367c303436a1c5d07035f2397af358208
nikhl1025 <nikhilshrivastava1025@gmail.com>
2022-09-21 17:11:25 +0530

    init: final-setup

--- db.js
+++
@@ -1,7 +0,0 @@
-const products = require("./products.json");
-const cart = require("./cart.json");
-
-module.exports = {
-    products,
-    cart
-}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
-const port = 8000;
+const port = 3000;
 
 server.use(middlewares);
 server.use(router);
