7cf5892e054f078b8710d6eb2dd6d4609db16254
nikhl1025 <nikhilshrivastava1025@gmail.com>
2022-09-21 17:04:01 +0530

    init: final-setup_2

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const data = require('./db.js');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(data);
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
-
 const port = 8000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => console.log("ready on " + port));
+
+server.listen(port);
